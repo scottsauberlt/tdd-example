9851abbb30a3879d478dddb8d055dba3c2169ff5
scottsauberlt <83788907+scottsauberlt@users.noreply.github.com>
2024-04-15 09:00:52 -0500

    remove validation (#1)

--- src/tddexample.web.client/src/App.spec.jsx
+++ src/tddexample.web.client/src/App.spec.jsx
@@ -9,32 +9,9 @@ it('should show success message when submitting form', async () => {
     const firstNameInput = screen.getByLabelText("First Name");
     await userEvent.type(firstNameInput, 'Spongebob');
 
-    const lastNameInput = screen.getByLabelText("Last Name");
-    await userEvent.type(lastNameInput, 'Squarepants');
-
     const button = screen.getByRole('button');
     await userEvent.click(button);
 
     const message = screen.getByText('Submitted successfully');
     expect(message).toBeInTheDocument();
-});
-
-it('should show error message when submitting form without entering in first name', async () => {
-    render(<App/>);
-
-    const button = screen.getByRole('button');
-    await userEvent.click(button);
-
-    const errorMessage = screen.getByText('First Name is required');
-    expect(errorMessage).toBeInTheDocument();
-});
-
-it('should show error message when submitting form without entering in last name', async () => {
-    render(<App/>);
-
-    const button = screen.getByRole('button');
-    await userEvent.click(button);
-
-    const errorMessage = screen.getByText('Last Name is required');
-    expect(errorMessage).toBeInTheDocument();
 });
