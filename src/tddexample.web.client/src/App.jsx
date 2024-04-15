eb86fc7a0be48a790757e1004c65b6b516126b7e
Scott Sauber <ssauber@leantechniques.com>
2024-04-15 08:41:13 -0500

    remove validation

--- src/tddexample.web.client/src/App.jsx
+++ src/tddexample.web.client/src/App.jsx
@@ -15,17 +15,7 @@ function App() {
             {message && (<div className="alert alert-success">{message}</div>)}
             <div>
                 <label htmlFor="first-name-input">First Name</label>
-                <input id="first-name-input" {...register("firstName", {required: "First Name is required"})} />
-                {errors.firstName && errors.firstName.type === "required" && (
-                    <div className="text-danger">First Name is required</div>
-                )}
-            </div>
-            <div>
-                <label htmlFor="last-name-input">Last Name</label>
-                <input id="last-name-input" {...register("lastName", {required: "Last Name is required"})} />
-                {errors.lastName && errors.lastName.type === "required" && (
-                    <div className="text-danger">Last Name is required</div>
-                )}
+                <input id="first-name-input" {...register("firstName")} />
             </div>
             <button>Submit</button>
         </form>
